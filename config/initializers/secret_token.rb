beef860b62e821dd6d9eec8783c2ba918fa04f57
petejh <petejh@truly.fake>
2014-02-27 06:32:24 +0000

    Create messages project
    From Chapters 24 and 25 of The RSpec Book (Chelimsky).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Messages::Application.config.secret_key_base = 'd11fd2e25767fca56d42a6b412852f8c9f761831494bca293ea27bc0019981ffd7067c5f9ae7183bc0d90432f23335e83970f9f80168831048ba7a1c5e063cfe'
