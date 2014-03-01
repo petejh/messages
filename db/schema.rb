1faa44eb0448967f9ce9585c8fcb9f80079c6904
petejh <petejh@truly.fake>
2014-03-01 03:22:58 +0000

    Re-generate the (previously empty) Message model with new attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140228021332) do
+ActiveRecord::Schema.define(version: 20140301032021) do
 
   create_table "messages", force: true do |t|
+    t.string   "title"
+    t.text     "text"
+    t.integer  "recipient_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
